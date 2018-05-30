12bca926e7d605561afafd836f4b40f341b17385
climba <mike@joomla-go.com>
2018-05-30 13:32:53 -0700

    Fixed some issues in the supervisor area

--- supervisor.js
+++ supervisor.js
@@ -70,19 +70,16 @@ function salesById() {
       message: "Enter the Item ID that you would like to see sales for:",
     
     }).then(function(answer){
-      var query = "SELECT order_num, department_name, order_quantity FROM orders WHERE order_prod_id = ?";
+      var query = "SELECT item_id, product_name, department_name, SUM(product_sales * products.price) FROM products WHERE item_id = ?";
       connection.query(query, [answer.action], 
         function(err, res) {
           if (err) {
             console.log(err);
           }
           // console.log(res);
-          for(var i =0; i < res.length; i++) {
-          console.log("Order Number: " + res[i].order_num +
-          " || Department: " + res[i].department_name +
-          " || Order Quantity: " + res[i].order_quantity  );
-        }
-
+          console.log("Total sales for " + res[0].product_name +
+                      " is: $" + res[0]['SUM(product_sales * products.price)'] +
+                      " it's department is " + res[0].department_name );
       }
     )
     connection.end();
@@ -92,14 +89,78 @@ function salesById() {
 }
 
 
-// function salesByDept() {
-//       // Get all rows in a Department
-//       // Multiply sales by price for each item ID
-//       // Add the vales together
-//       // connection.query("SELECT department_name,  SUM(product_sales * price) AS TOTAL FROM products GROUP BY department_name", function(err, res) {
-//         connection.query("SELECT department_name SUM(product_sales * price) AS total_prod_sales, department_name, FROM products GROUP BY department_name ORDER BY total_profit ASC", function(err, res) {
-//         if (err) throw err;
-//         console.log(res)
-        
-//       });
-// }
+function salesByDept() {
+      // Get all rows in a Department
+      // Multiply sales by price for each item ID
+      // Add the vales together
+       connection.query("SELECT department_name, SUM(product_sales * products.price) FROM products GROUP BY department_name ORDER BY item_id ASC;", function(err, res) {
+        if (err) throw err;
+        // console.log(res)
+        for(var i =0; i < res.length; i++) {
+          console.log("The total profits of the " + res[i].department_name + " department are: $" + res[i]['SUM(product_sales * products.price)'])
+        }      
+      })
+      connection.end();
+}
+
+function addProducts() {
+  inquirer
+  .prompt([
+    {
+    name: "product_name",
+    type: "input",
+    message: "Enter a new product name",
+    },
+    {
+    name: "department_name",
+    type: "input",
+    message: "Enter it's department name?",
+    },
+    {
+    name: "price",
+    type: "input",
+    message: "Enter it's price",
+    },
+    {
+    name: "shipping",
+    type: "input",
+    message: "Enter it's shipping cost",
+    },
+    {
+    name: "stock_quantity",
+    type: "input",
+    message: "Enter a begining stock quantity",
+    },
+    {
+    name: "wholesale_cost",
+    type: "input",
+    message: "Enter it's wholesale cost",
+    }
+    ]).then(function (answers) {
+      addNewItem(answers.product_name, answers.department_name, answers.price, answers.shipping, answers.stock_quantity, answers.wholesale_cost);
+    })
+}
+
+function addNewItem(product_name, department_name, price, shipping, stock_quantity, wholesale_cost) {
+  console.log("Inserting a new item.... \n");
+  var query = connection.query(
+    "INSERT INTO products SET ?",
+    {
+      product_name,
+      department_name,
+      price,
+      shipping,
+      stock_quantity,
+      wholesale_cost,
+    },
+    function (err, res) {
+      if (err) {
+        console.log(err);
+      }
+      // console.log(res);
+    }
+  );
+  // logs the actual query being run
+  console.log(query.sql);
+  connection.end();
+}
